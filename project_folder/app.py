8942ba0fb949b0b3eb79d35397a1e7a5ab2216d1
varshneyKrishna <22f3000605@ds.study.iitm.ac.in>
2025-02-03 19:30:15 +0530

    created an organized folder for the project

---
+++ project_folder/app.py
@@ -0,0 +1,43 @@
+from flask import Flask
+from application.extensions import db, security
+import init_db
+import application.views as views
+from flask_security import SQLAlchemyUserDatastore
+from application.resources import api
+
+def create_app():
+    app = Flask(__name__)
+
+    app.config['DEBUG'] = True
+    app.config['SECRET_KEY'] = 'mysecretkey'
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    app.config['SECURITY_PASSWORD_SALT'] = 'mysaltypassword'
+    app.config['WTF_CSRF_ENABLED'] = False
+    app.config['SECURITY_TOKEN_AUTHENTICATION_HEADER'] = 'Authentication'
+
+    db.init_app(app)
+
+    with app.app_context():
+        from application.models import User, Role, Base
+
+        datastore = SQLAlchemyUserDatastore(db, User, Role)
+        security.init_app(app, datastore)
+        
+        try:
+            print("Creating all tables in the database...")
+            Base.metadata.create_all(db.engine)  
+        except Exception as e:
+            print(f"Error while creating tables: {e}")
+
+        init_db.create_data(datastore)
+
+    views.create_view(app, datastore, db)
+    api.init_app(app)
+    return app
+
+
+app = create_app()
+
+if __name__ == '__main__':
+    app.run()
